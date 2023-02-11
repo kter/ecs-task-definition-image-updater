1ddecf59bcb03cf72256005377702063237c4da8
Tomohiko Takahashi <takahashi@tomohiko.io>
2023-02-11 12:53:30 +0900

    TestInitializeAWSSession

--- main_test.go
+++ main_test.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"testing"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/endpoints"
 )
 
 func TestValidArguments(t *testing.T) {
@@ -15,5 +18,23 @@ func TestInvalidArguments(t *testing.T) {
 	args := []string{"a", "b", "c"}
 	if validateArguments(args) {
 		t.Error("args validated when they should not have")
+	}
+}
+
+func TestInitializeAWSSession(t *testing.T) {
+	initializeAWSSessionResult, err := initializeAWSSession()
+
+	if err != nil {
+		t.Errorf("initializeAWSSession returned unexpected error %s", err.Error())
+	}
+
+	// Check that the result is not nil
+	if initializeAWSSessionResult == nil {
+		t.Errorf("initializeAWSSession did not return a session")
+	}
+
+	// Check that the region is correct
+	if initializeAWSSessionResult != nil && aws.StringValue(initializeAWSSessionResult.Config.Region) != endpoints.ApNortheast1RegionID {
+		t.Errorf("expected region to be %s, but was %s", endpoints.ApNortheast1RegionID, aws.StringValue(initializeAWSSessionResult.Config.Region))
 	}
 }
